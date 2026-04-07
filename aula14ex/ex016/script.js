adb6761326271385992bfae9ccd9ebe64ca9de5d
guilhermejoaquimramos <259980026+guilhermejoaquimramos17@users.noreply.github.com>
2026-04-07 12:11:40 -0400

    modificação

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -7,15 +7,31 @@ function contar(){
     var passo = Number(npasso.value)
     var res = document.getElementById('res')
 
+    if (nini.value == 0 || nfim.value == 0 || npasso.value == 0){
+        res.innerHTML = 'Impossível contar.'
+        alert('Erro. Digite números válidos')
+    } 
+
     if (passo <= 0){
         passo = 1
         alert('Passo 0 não é válido, considerando passo 1')  
-    }
+    } 
 
-    res.innerText = 'Contando: '
-    for (inicio; inicio <= fim; inicio = inicio + passo){
+    res.innerHTML = 'Contando: <br>'
+    if (inicio < fim){
+        // Contagem crescente
+       for (inicio; inicio <= fim; inicio += passo){
         res.innerHTML += `${inicio} &#x1F449 `
+        
+    } 
+    }else {
+        // Contagem regressiva
+        for(inicio; inicio >= fim; inicio -= passo){
+            res.innerHTML += `${inicio} &#x1F449 `
+        }
     }
+    
+    res.innerHTML += `\u{1F3C1}`
     
     
 }
