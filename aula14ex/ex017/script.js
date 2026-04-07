2b70a75be7a7c176241b810f2ff33f4c684022b4
guilhermejoaquimramos <259980026+guilhermejoaquimramos17@users.noreply.github.com>
2026-04-07 12:59:10 -0400

    atualização da tabuada

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,9 +1,19 @@
+
 function contar(){
     var n1 = document.getElementById('num')
-    var numTab = (n1.value)
-    var res = document.getElementById('res')
-    for (var c = 0; c <= 10; c++){
-        var mult = Number(c) * numTab
-        res.innerHTML += `${numTab} X ${c} = ${mult} <br>`
+    var tab = document.getElementById('tabuada')
+    if (n1.value == 0){
+        alert('Digite um número')
+        res.innerHTML = 'Impossível contar'
+    } else {
+        var numTab = (n1.value)
+        tab.innerHTML = ''
+        for (var c = 0; c <= 10; c++){
+        var item = document.createElement('option')
+        item.text = `${numTab} X ${c} = ${numTab*c}`
+        item.value = `tab${c}`
+        tab.appendChild(item)
     }
+    }
+    
 }
